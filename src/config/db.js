fa6bc0a4ae88bf7e4759d9213cbed2647ed2bfbe
kajal-rajodiya <kajalrajodiya1@gmail.com>
2025-03-19 23:06:19 +0530

    git

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,31 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async ()=>{
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URI)
+//         console.log('connect to db')
+//     } catch (error) {
+//         console.log("error connectiong to DB",error)
+//     }
+// }
+// export default connectDB;
+
+
+// const mongoose = require('mongoose');
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
+const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log('connect to db')
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000 // Increase timeout to 30 seconds
+        });
+        console.log('Connected to MongoDB');
     } catch (error) {
-        console.log("error connectiong to DB",error)
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1);
     }
-}
+};
 export default connectDB;
+// module.exports = connectDB;
