cf1e847a0e764913a6f55a717942b44b8b0f3ce1
kajal-rajodiya <kajalrajodiya1@gmail.com>
2025-03-20 13:05:18 +0530

    db

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log('connect to db')
-    } catch (error) {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
+            bufferCommands: false // Disable mongoose buffering
+          }); 
+           console.log("connecct to db")
+        }catch (error) {
         console.log("error connectiong to DB",error)
     }
 }
